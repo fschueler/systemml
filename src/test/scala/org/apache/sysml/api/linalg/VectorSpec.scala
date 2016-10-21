67d1ad353541578d977500147ee7a5f8e0f72597
Felix Schueler <felix.schueler@ibm.com>
2016-10-20 18:13:54 -0700

    [SCALA-DSL] WIP version of the DSL

---
+++ src/test/scala/org/apache/sysml/api/linalg/VectorSpec.scala
@@ -0,0 +1,15 @@
+package org.apache.sysml.api.linalg
+
+import org.apache.sysml.api.BaseAPISpec
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class VectorSpec extends BaseAPISpec {
+
+  "A Vector" - {
+
+    "can be transposed" is pending
+  }
+
+}
