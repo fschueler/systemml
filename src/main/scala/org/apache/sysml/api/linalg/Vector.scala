cec1b1b7fb65e8258ed7f19858e4e47c7485b3bc
Felix Schueler <felix.schueler@ibm.com>
2017-01-31 18:25:52 -0800

    Add missing builtin and matrix types

---
+++ src/main/scala/org/apache/sysml/api/linalg/Vector.scala
@@ -0,0 +1,24 @@
+package org.apache.sysml.api.linalg
+
+object Vector {
+  /**
+    * Create a Matrix with one column
+    * @param values
+    * @return
+    */
+  def apply(values: Array[Double]): Matrix = {
+    Matrix(values, values.length, 1)
+  }
+
+  def rand(length: Int): Matrix = {
+    Matrix.rand(length, 1)
+  }
+
+  def zeros(length: Int): Matrix = {
+    Matrix.zeros(length, 1)
+  }
+
+  def ones(length: Int): Matrix = {
+    Matrix.ones(length, 1)
+  }
+}
