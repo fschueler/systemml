d5f227622a5b866e0ef01ef0f4ac55879bc7a63f
fschueler <fschueler@posteo.de>
2017-02-18 19:41:29 -0800

    Fix matrixmetadata

--- src/main/scala/org/apache/sysml/api/linalg/Matrix.scala
+++ src/main/scala/org/apache/sysml/api/linalg/Matrix.scala
@@ -23,7 +23,7 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.types.{DoubleType, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import org.apache.sysml.api.linalg.api.:::
-import org.apache.sysml.api.mlcontext.{BinaryBlockMatrix, MLContext, MLContextConversionUtil, MatrixMetadata}
+import org.apache.sysml.api.mlcontext.{MatrixMetadata, _}
 import org.apache.sysml.parser.Expression.ValueType
 import org.apache.sysml.runtime.controlprogram.caching.MatrixObject
 import org.apache.sysml.runtime.controlprogram.context.SparkExecutionContext
@@ -163,7 +163,7 @@ class Matrix protected(val nrow: Int,
 
   protected[sysml] def getMatrixMetadata(): MatrixMetadata = {
     matob match {
-      case Some(mo) => mo.getMetaData.asInstanceOf[MatrixMetadata]
+      case Some(mo) => new MatrixMetadata(mo.getMatrixCharacteristics)
       case None     => throw new RuntimeException("Matrix has not been evaluated in SystemML - can not create MatrixMetaData")
     }
   }
